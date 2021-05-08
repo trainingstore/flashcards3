1807c67b0f413348af6d564bd999319c999021be
Serge <foravitosells@gmail.com>
2021-05-08 20:53:36 +0300

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+  skip_before_action :require_login, only: [:index, :new, :create]
+
+
+  def index
+    @users = User.all
+  end
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def edit; end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      auto_login @user
+      flash[:success] = 'Account was successfully created'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'Account was successfully updated'
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = 'Account was successfully destroyed'
+    redirect_to root_path
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
+end
